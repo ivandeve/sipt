61f64aecbaf7ea58d78ddc911252a322e1287ce3
informastudios <informastudios@gmail.com>
2022-01-17 16:16:00 +0700

    change css

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
-import Login from './page/auth/login';
+import logo from "./logo.svg";
+import "./App.css";
+import Login from "./page/auth/login";
+import { ChakraProvider } from '@chakra-ui/react'
 
 function App() {
   return (
     <div className="App">
-      <Login/>
+      <ChakraProvider>
+        <Login />
+      </ChakraProvider>
     </div>
   );
 }
